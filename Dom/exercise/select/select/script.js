f752987ccf8a8ac6e5a6b0dae205e53942556261
Buynaa17 <151522645+Buynaa17@users.noreply.github.com>
2023-12-26 19:37:11 +0800

    to do

---
+++ Dom/exercise/select/select/script.js
@@ -0,0 +1,16 @@
+// 1. Use getElementById to select an element by ID
+
+const e = document.getElementById("item");
+console.log(e);
+
+// 2. Use getElementsByName to select elements by name
+
+const l = document.getElementsByName("username");
+console.log(l);
+
+// 3. Use getElementsByTagName to select elements by tag name
+
+// 4. Use getElementsByClassName to select elements by class name
+
+const i = document.getElementsByClassName("item");
+console.log(i[0]);
