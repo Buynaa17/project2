535dc5083bb157c6a821c8e8f4c1a552fb4d2fab
Buynaa17 <151522645+Buynaa17@users.noreply.github.com>
2024-01-08 19:25:24 +0800

    tododrop zuuv

--- project.js
+++ project.js
@@ -1,21 +1,40 @@
-const tasks = [];
+const task = [];
 
-const addTask = () => {
-  const tasksElement = document.getElementById("Box").value;
-  console.log(tasksElement);
-  const task = {
-    title: "end title dudaad og",
-    desc: "end desc duudaad ug",
-  };
-  tasksElement = `<div class = "task"> ${tasksElement}</div>`;
-  tasks.push();
-  console.log(tasks);
-};
-
-// const v = () => {
-//   const Title = document.getElementById("Title");
-//   const valuee = Title.value;
+// const addTask = () => {
+//   const tasksElement = document.getElementById("Box").value;
+//   console.log(tasksElement);
+//   const task = ` <div> ${Title}, ${Desc} </div>`;
+//   tasksElement = `<div class = "task"> ${tasksElement}</div>`;
+//   tasks.push();
+//   console.log(tasks);
+//   todoElement.innerHTML = todoElement.innerHTML + taskHtml;
 // };
+
+let currentTarget = "Box";
+let generatedId = 0;
+
+function renderTask() {
+  const taskElement = document.getElementById(currentTarget);
+  let result = "";
+
+  for (let i = 0; i < task.length; i++) {
+    const task = task[i];
+
+    const taskHtml = `<div>
+    ${task.title}, ${Desc}, ${Priority}
+    <button onclick=" removeTask(${i})">remove</button>
+    </div>
+    `;
+    result += taskHtml;
+  }
+  taskElement.innerHTML = result;
+}
+
+function addTask() {
+  const title = document.getElementById("taskTitle").value;
+  const description = document.getElementById("taskDescription").value;
+  const priority = document.getElementById("taskPriority").value;
+}
 
 function showModal() {
   const modal = document.getElementById("createModal");
