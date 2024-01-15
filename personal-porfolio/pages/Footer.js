22443d20bc0b5f752ae5137b371901e1e2fec27b
Buynaa17 <151522645+Buynaa17@users.noreply.github.com>
2024-01-15 19:26:51 +0800

    react 1

---
+++ personal-porfolio/pages/Footer.js
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <div>
+      <h1> Sain </h1>
+      <h1> Sain uu</h1>
+      <h1> ?</h1>
+    </div>
+  );
+}
--- personal-porfolio/pages/index.js
+++ personal-porfolio/pages/index.js
@@ -1,7 +1,28 @@
+import { Header } from "./header";
+import { Footer } from "./footer";
 export default function Home() {
   return (
-    <div>
+    <div style={{ background: "yellow" }}>
+      <p>Hello world</p>
+      <Header />
+      <Footer />
       <p>Hello world</p>
     </div>
   );
 }
+// function Header() {
+//   return (
+//     <div>
+//       <h1> aaaz</h1>
+//     </div>
+//   );
+// }
+// function Footer() {
+//   return (
+//     <div>
+//       <h1> e </h1>
+//       <h1> eej</h1>
+//       <h1> eejee</h1>
+//     </div>
+//   );
+// }
