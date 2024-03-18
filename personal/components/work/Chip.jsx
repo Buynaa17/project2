a58cfa7e70b74a19bbb74458a48f3f270449e944
Buynaa17 <151522645+Buynaa17@users.noreply.github.com>
2024-03-18 19:28:06 +0800

    yhuu

--- personal/components/work/Chip.jsx
+++
@@ -1,7 +0,0 @@
-export function Chip({ label }) {
-  return (
-    <div className="flex  px-4 py-2 bg-slate-200  rounded-full  text-gray-700   flex-col  gap-2 items-center  justify-center  ">
-      {label}
-    </div>
-  );
-}
